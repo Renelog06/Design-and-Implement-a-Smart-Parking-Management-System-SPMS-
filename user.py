e1c1d4b1acc896dbc05c56340928482e26c20829
Eriius <minhtrungumtkg@gmail.com>
2025-06-27 23:01:57 +0700

    update

--- user.py
+++ user.py
@@ -24,6 +24,17 @@ class User:
             return User(username, cls.users[username]["role"])
         else:
             return None
+    def admin_menu():
+        print("1. Manage slots")
+        print("2. Set hourly rates")
+        print("3. Export revenue reports")
+    def attendant_menu():
+        print("1. Check-in vehicles")
+        print("2. Check-out vehicles")
+        print("3. Update slots")
+    def owner_menu():
+        print("1. View available slots")
+        print("2. Pay parking fees")
 
 u = input("Enter username: ")
 p = input("Enter password: ")
@@ -31,5 +42,13 @@ p = input("Enter password: ")
 data = User.login(u, p)
 if data:
     print(f"Login successful! Welcome {data.username} ")
+    print(f"Your role is: {data.role}")
+    if data.role == "admin":
+        User.admin_menu()
+    elif data.role == "attendant":
+        User.attendant_menu()
+    elif data.role == "owner":
+        User.owner_menu()
 else:
     print("Login failed! Please check your username and password.")
+
